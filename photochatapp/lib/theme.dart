5e7374adf640e7a25fd0c91b5eca5821d4f4a4ed
tianhaoz95 <tianhaoz@umich.edu>
2020-02-11 21:54:56 -0800

    Update theme.dart

--- photochatapp/lib/theme.dart
+++ photochatapp/lib/theme.dart
@@ -3,20 +3,14 @@ import 'package:flutter/material.dart';
 ThemeData theme = ThemeData(
   primarySwatch: Colors.amber,
   inputDecorationTheme: InputDecorationTheme(
-    contentPadding: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.all(
-        Radius.circular(8.0)
-      ),
-    )
-  ),
+      contentPadding: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      )),
   buttonTheme: ButtonThemeData(
-    buttonColor: Colors.amber,
-    textTheme: ButtonTextTheme.primary,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(
-        Radius.circular(8.0)
-      ),
-    )
-  ),
+      buttonColor: Colors.amber,
+      textTheme: ButtonTextTheme.primary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      )),
 );
