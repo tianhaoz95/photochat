b557401cba60d150d8e949cafd2980c0b629a9e2
tianhaoz95 <tianhaoz@umich.edu>
2020-02-13 19:27:56 -0800

    add apple icon

--- photochatapp/test/widget_test.dart
+++ photochatapp/test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:photochatapp/app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(PhotoChatApp());
   });
 }
