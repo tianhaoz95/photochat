ad84f9246174ee647290baf7d50afbfcbb57c063
tianhaoz95 <tianhaoz@umich.edu>
2020-02-15 11:54:53 -0800

    Update screen_adapter.dart

--- photochatapp/lib/components/screen_adapter/screen_adapter.dart
+++ photochatapp/lib/components/screen_adapter/screen_adapter.dart
@@ -5,11 +5,17 @@ class ScreenAdapter extends StatelessWidget {
   ScreenAdapter({this.child});
   @override
   Widget build(BuildContext context) {
+    double aspectRatio =
+        MediaQuery.of(context).size.width / MediaQuery.of(context).size.height;
+    double preferredAspectRatio = 10.0 / 16.0;
+    if (aspectRatio > preferredAspectRatio) {
+      aspectRatio = preferredAspectRatio;
+    }
     return Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          AspectRatio(aspectRatio: 9.0 / 16.0, child: this.child),
+          AspectRatio(aspectRatio: aspectRatio, child: this.child),
         ],
       ),
     );
