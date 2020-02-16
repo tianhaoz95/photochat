886d6b8f142d4164e68a78b0cf51b3d583a45620
tianhaoz95 <tianhaoz@umich.edu>
2020-02-16 00:39:24 -0800

    Update encode_to_result.dart

--- photochatapp/test_driver/routines/encode_to_result.dart
+++ photochatapp/test_driver/routines/encode_to_result.dart
@@ -1,23 +1,39 @@
 import 'package:flutter_driver/flutter_driver.dart';
 
-// import '../config/logger.dart';
+import '../config/logger.dart';
 
 // import '../utilities/screenshot.dart';
 
 Future<void> checkEncodeToResult(FlutterDriver driver) async {
+  String encodeToResultTestLoggingNamespace = 'encode_screen_integration_test';
+  logger.info('try to tap message input',
+      name: encodeToResultTestLoggingNamespace);
   await driver.tap(find.byValueKey('encode_screen_msg_input'));
+  logger.info('try to type in message',
+      name: encodeToResultTestLoggingNamespace);
   await driver.enterText('my awesome message');
   await Future.delayed(const Duration(seconds: 1));
+  logger.info('try to tap checkbox to enter token',
+      name: encodeToResultTestLoggingNamespace);
   await driver.tap(find.byValueKey('encode_screen_token_checkbox'));
   await Future.delayed(const Duration(seconds: 1));
+  logger.info('try to tap token input',
+      name: encodeToResultTestLoggingNamespace);
   await driver.tap(find.byValueKey('encode_screen_token_input'));
   await Future.delayed(const Duration(seconds: 1));
+  logger.info('try to enter token', name: encodeToResultTestLoggingNamespace);
   await driver.enterText('my awesome token');
   await Future.delayed(const Duration(seconds: 1));
+  logger.info('scroll down to find send to encode button',
+      name: encodeToResultTestLoggingNamespace);
   await driver.scrollUntilVisible(
       find.byValueKey('encode_screen_scrollable_list'),
       find.byValueKey('encode_screen_encode_btn'));
+  logger.info('try to tap send to encode button',
+      name: encodeToResultTestLoggingNamespace);
   await driver.tap(find.byValueKey('encode_screen_encode_btn'));
+  logger.info('shoud have landed to encode result screen',
+      name: encodeToResultTestLoggingNamespace);
   await Future.delayed(const Duration(seconds: 1));
   await driver.tap(find.pageBack());
   await Future.delayed(const Duration(seconds: 1));
