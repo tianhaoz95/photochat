fd4d1dcc3578ffccf4f5ee79a0a9f9913fd1d892
tianhaoz95 <tianhaoz@umich.edu>
2020-02-16 00:21:08 -0800

    Update encode_to_result.dart

--- photochatapp/test_driver/routines/encode_to_result.dart
+++ photochatapp/test_driver/routines/encode_to_result.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_driver/flutter_driver.dart';
 
+// import '../config/logger.dart';
+
 // import '../utilities/screenshot.dart';
 
 Future<void> checkEncodeToResult(FlutterDriver driver) async {
-  await driver.tap(find.byValueKey('decode_screen_msg_input'));
+  await driver.tap(find.byValueKey('encode_screen_msg_input'));
   await driver.enterText('my awesome message');
   await Future.delayed(const Duration(seconds: 1));
   await driver.tap(find.byValueKey('encode_screen_token_checkbox'));
