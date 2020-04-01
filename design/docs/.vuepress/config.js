4da29c4b0448d244a1e56e139059bdf4ebcceb1c
Tianhao Zhou <tianhaoz@umich.edu>
2020-04-01 16:49:36 -0700

    dev: init vuepress design docs site (#82)

---
+++ design/docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Mini Donkey Design Docs',
+      description: 'Home for all Mini Donkey design docs'
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: '小驴za设计文档',
+      description: '小驴za设计文档汇总'
+    }
+  },
+  themeConfig: {
+    logo: 'donkey_logo_512_rounded.png',
+    locales: {
+      '/': {
+        selectText: 'Languages',
+        label: 'English'
+      },
+      '/zh/': {
+        selectText: '选择语言',
+        label: '简体中文'
+      }
+    }
+  }
+}
