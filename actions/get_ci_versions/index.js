243990d26678887ab66146cf163a8850483c1d28
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-12 23:45:38 -0700

    chore: use app instead

--- actions/get_ci_versions/index.js
+++
@@ -1,7 +0,0 @@
-const core = require('@actions/core');
-
-const target_flutter_version = '1.22.1'
-const target_flutter_channel = 'stable'
-
-core.setOutput("flutter_version", target_flutter_version);
-core.setOutput("flutter_channel", target_flutter_channel);
